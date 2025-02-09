51e95d5106c9ad7e6336e2657553ab2284a625f5
AdenGoBrrrr <adenalhamis2006@gmail.com>
2025-02-09 15:44:47 +0000

    Developed Classes in characters and classes. imported all other files into builder

--- characters.py
+++ characters.py
@@ -1,2 +1,55 @@
+import random
+
+
 class Character:
-    pass
+
+    full_spell_casters = ["Wizard", "Sorcerer", "Bard", "Cleric", "Druid"]
+    half_spell_casters = ["Paladin", "Ranger", "Artificer"]
+    third_spell_casters = ["Eldritch Knight", "Arcane Trickster"]
+    
+    def __init__(self, name, class_chosen, strength, dex, con, intelligence, wis, cha):
+        self.name = name
+        self.__max_hp = class_chosen.hit_die() + self.ability_scores["con"]
+        self.level = 1
+        self.classes = {class_chosen: 1}
+        self.sub_classes = {class_chosen: None}
+        self.ability_scores = {"str": strength, "dex": dex, "con": con, "int": intelligence, "wis": wis, "cha": cha}
+        if class_chosen.name == "Warlock":
+            self.warlock_spell_slots = class_chosen.spell_slots_by_level(1)
+            self.spell_slots = [0,0,0,0,0,0,0,0,0]
+        else:
+            self.spell_slots = class_chosen.spell_slots_by_level(1)
+            self.warlock_spell_slots = [0,0,0,0,0]
+        
+
+    def level_up(self, class_chosen):
+        self.level += 1
+        self.classes[class_chosen] += 1
+        self.__max_hp += random.randint(class_chosen.hit_die())
+
+        if self.classes[class_chosen] == 3:
+            print(f"choose a subclass for {class_chosen.name}:\n")
+            for subclass in class_chosen.subclasses:
+                print(f"- {subclass.name}\n")
+            while True:
+                subclass_chosen = input()
+                if subclass_chosen in class_chosen.subclasses:
+                    self.sub_classes[class_chosen] = subclass_chosen
+                    break
+                else:
+                    print("Invalid subclass, please choose a subclass from the list\n")
+
+        if class_chosen.name in self.full_spell_casters:
+            # add all spell slots together 
+            pass
+        elif class_chosen.name in self.half_spell_casters:
+            # Add half of the spell slots together rounding down
+            pass
+        else:
+            if self.sub_classes.items in self.third_spell_casters :
+                # Use spell slots from the subclass
+                pass
+
+    @property
+    def max_hp(self):
+        return self.__max_hp
--- classes.py
+++ classes.py
@@ -1,9 +1,32 @@
 class Class:
+
+    warlock_spell_level_per_level = {1: 1,
+                                     2: 1,
+                                     3: 2,
+                                     4: 2,
+                                     5: 3,
+                                     6: 3,
+                                     7: 4,
+                                     8: 4,
+                                     9: 5,
+                                     10: 5,
+                                     11: 5,
+                                     12: 5,
+                                     13: 5,
+                                     14: 5,
+                                     15: 5,
+                                     16: 5,
+                                     17: 5,
+                                     18: 5,
+                                     19: 5,
+                                     20: 5}
     
-    def __init__(self, name, hit_die, spell_slots_by_level, armour_proficiencies, weapon_proficiencies, tool_proficiencies, saving_throws_proficiencies, skills_proficiencies):
-        self.name = name
+    def __init__(self, name, hit_die, spell_slots_by_level, spell_levels_by_level, warlock_spell_slots_by_level, armour_proficiencies, weapon_proficiencies, tool_proficiencies, saving_throws_proficiencies, skills_proficiencies):
+        self.__name = name
         self.__hit_die = hit_die
         self.__spell_slots_by_level = spell_slots_by_level
+        self.__warlock_spell_slots_by_level = warlock_spell_slots_by_level
+        self.__spell_levels_by_level = spell_levels_by_level
         self.subclasses = []
         self.spells = []
         self.cantrips = []
@@ -28,11 +51,20 @@ class Class:
         self.starting_equipment.append(equipment)
 
     @property
+    def name(self):
+        return self.__name
+
     def hit_die(self):
         return self.__hit_die
     
     def spell_slots_by_level(self):
         return self.__spell_slots_by_level
+    
+    def warlock_spell_slots_by_level(self):
+        return self.__warlock_spell_slots_by_level
+
+    def spell_level_by_level(self):
+        return self.__spell_levels_by_level
     
     def __str__(self):
         string = f"{self.name}:\n subclasses: {self.subclasses}\n cantrips: {self.cantrips}\n spells: {self.spells}\n features: {self.features}\n armour proficiencies: {self.armour_proficiencies}\n weapon proficiencies: {self.weapon_proficiencies}\n tool proficiencies: {self.tool_proficiencies}\n saving throws proficiencies: {self.saving_throws_proficiencies}\n skills proficiencies: {self.skills_proficiencies}\n starting equipment: {self.starting_equipment}"
@@ -40,7 +72,22 @@ class Class:
 
 
 class SubClass:
-    pass
+    
+    def __init__(self, name, features):
+        self.__name = name
+        self.spells = []
+        self.cantrips = []
+        self.features = features
+
+    @property
+    def name(self):
+        return self.__name
 
 class ClassFeature:
-    pass
+    
+    ## This will be expanded on later
+
+    def __init__(self, name, description, required_level):
+        self.name = name 
+        self.description = description
+        self.level_required = required_level
